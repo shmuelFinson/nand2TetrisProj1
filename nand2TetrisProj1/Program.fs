daf238acd367e0adaef05d19b1c263a60a77415e
Sam <34210553+SamF@users.noreply.github.com>
2020-11-02 19:22:50 +0200

    added some of the push add and sub functions

--- nand2TetrisProj1/Program.fs
+++ nand2TetrisProj1/Program.fs
@@ -5,29 +5,44 @@
 open System
 open System.IO
 
-type Color =
-        | push = 0
-        | pop = 1
+
 
 
 [<EntryPoint>]
 let main argv =
    
+   //pop functions:
 
-    
-    
+    //pop segment(local/argument/this/that) offset  - pop #1
 
-    //let checkType command = 
-      
+    //pop segment(pointer/temp) offset - pop #2
 
-    //translate a push command into HACK
-    let pushHackCommand (lineOfCode:string) = 
-        let hackCode = lineOfCode.Split([|" "|], StringSplitOptions.None)
-        let sourceReg = hackCode.[1] //name of register 
-        let address = hackCode.[2] //address in register
+    //pop static offset - pop #3
+
+    //pop constant offset - pop #4
+
+    // push functions:
+
+    //push segment(local/argument/this/that) offset  - push #1
+    let push_Lcl_Arg_This_That_Offset (register:string , offset:string) = 
+        printf "@%A\n" offset
+        printf "D=A\n"
+        printf "@%A\n" register
+        printf "A=M+D\n"
+        printf "D=M\n"
+        printf "@SP\n"
+        printf "A=M\n"
+        printf "M=D\n"
+        printf "@SP\n"
+        printf "M=M+1\n"
+
+    //push (pointer/temp) offset  - push #2
+
+    //push static offset - push #3
+    let push_static_offset(register:string , offset:string) = 
         printf "\n"
-        printf "//push %A %A \n"  sourceReg address
-        printf "@%A\n" address
+        printf "//push %A %A \n"  register offset
+        printf "@%A\n" offset
         printf "D=A\n"
         printf "@SP\n"
         printf "A=M\n"
@@ -35,6 +50,27 @@ let main argv =
         printf "@SP\n"
         printf "M=M+1\n"
         printf "\n"
+    
+    //push constant value - push #4
+
+
+
+
+    //translate a push command into HACK
+    let pushHackCommand (lineOfCode:string) = 
+        let hackCode = lineOfCode.Split([|" "|], StringSplitOptions.None) //push - 0 constant - 1 2 - 2
+        let sourceReg = hackCode.[1] //name of register 
+        let offset = hackCode.[2] //address in register
+
+        match sourceReg with
+                    |"local" ->  push_Lcl_Arg_This_That_Offset (sourceReg, offset)
+                    |"argument" -> push_Lcl_Arg_This_That_Offset (sourceReg, offset)
+                    |"this" -> push_Lcl_Arg_This_That_Offset (sourceReg, offset)
+                    |"that" -> push_Lcl_Arg_This_That_Offset (sourceReg, offset)
+                    |"static" -> push_static_offset (sourceReg, offset)
+                    |_ ->  printf "Unknown\n"
+
+       
 
 
     //translate a pop command into HACK
@@ -49,11 +85,45 @@ let main argv =
        // let hackCode = lineOfCode.Split([|" "|], StringSplitOptions.None)
        // let a = hackCode.[1]
        // let b = hackCode.[2]
-        printf "add\n"
+        printf "//add\n"
+        printf "@SP\n"
+        printf "M=M-1\n"
+        printf "@SP\n"
+        printf "A=M\n"
+        printf "D=M\n"
+        printf "@SP\n"
+        printf "M=M-1\n"
+        printf "@SP\n"
+        printf "A=M\n"
+        printf "D=D+A\n"
+        printf "@SP\n"
+        printf "A=M\n"
+        printf "M=D\n"
+        printf "@SP\n"
+        printf "M=M+1\n"
+
+
 
     //translate a sub command into HACK
     let subHackCommand (lineOfCode:string) = 
-              printf "sub\n"
+              printf "//sub\n"
+              printf "@SP\n"
+              printf "M=M-1\n"
+              printf "@SP\n"
+              printf "A=M\n"
+              printf "D=M\n"
+              printf "@SP\n"
+              printf "M=M-1\n"
+              printf "@SP\n"
+              printf "A=M\n"
+              printf "A=M\n"
+              printf "D=A-D\n"
+              printf "@SP\n"
+              printf "A=M\n"
+              printf "M=D\n"
+              printf "@SP\n"
+              printf "M=M+1\n"
+              
 
     //read the file into a string
     let lines = File.ReadAllLines(@"C:\Users\Shmuel Finson\Desktop\aFile.txt")
